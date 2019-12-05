301a0402594a85ef0394491b4c2c5cf61cb90f7d
Pranav50 <pranavpw815@gmail.com>
2019-12-05 13:34:01 +0530

    All files added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Route, Switch } from 'react-router-dom'
+import {BrowserRouter, Route, Switch } from 'react-router-dom'
 import Home from './components/Home'
 import Rooms from './components/Rooms'
 import SingleRoom from './components/SingleRoom'
@@ -9,7 +9,7 @@ import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div>
+    <BrowserRouter basename='/BeachResort/'>
       <Navbar/>
         <Switch>
           <Route exact path="/" component={Home} />
@@ -17,7 +17,7 @@ function App() {
           <Route exact path="/rooms/:slug" component={SingleRoom} />
           <Route exact component={Error}/> 
         </Switch>
-    </div>  
+    </BrowserRouter>  
   );
 }
 
